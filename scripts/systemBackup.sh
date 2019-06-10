d0a9305c48609cd3c1cd6573bb0b5dc6d51baec1
d4rkeagle65 <50802414+d4rkeagle65@users.noreply.github.com>
2019-06-10 16:23:22 -0400

    Update systemBackup.sh

--- scripts/systemBackup.sh
+++ scripts/systemBackup.sh
@@ -1,11 +1,11 @@
 $backupsDir=/srv/backups
-$backupsStagingDir=${backupsDir}/stagingdir
+$backupsStagingDir="${backupsDir}/stagingdir"
 
 hostname=`hostname`
 timestamp=`date -Is`
 filename="sysBackup-${hostname}-${timestamp}.tar.gz"
 
 mkdir -p $backupsStagingDir
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","$backupsStagingDir"} / $backupsStagingDir
-tar -cvpzf ${$filename}.tar.gz --exclude="${backupsDir}/${filename}.tar.gz" $backupsStagingDir
+rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","${backupsStagingDir}/*"} / $backupsStagingDir
+tar -cvpzf "${backupsDir}/${$filename}.tar.gz" --exclude="${backupsDir}/${filename}.tar.gz" $backupsStagingDir
 rm -Rf $backupsStagingDir
