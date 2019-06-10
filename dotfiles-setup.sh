899bcf551fb0962309506d22b8db52854a5e34cd
David Hardin <dhardin@hardinsolutions.net>
2019-06-10 15:20:21 -0400

    Changed the rm lines in dotfiles-setup.sh to just be -f tacs for ln.

--- dotfiles-setup.sh
+++ dotfiles-setup.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/bash
 
-rm /home/${1}/.bash_aliases
-rm /home/${1}/.bash_functions
-rm /home/${1}/.bash_lessfilter
-rm /home/${1}/.bash_profile
-rm /home/${1}/.bashrc
-rm /home/${1}/.makepkg.i686.conf
-rm /home/${1}/.vimrc
+ln -sf /srv/git/d4rks-dotfiles/.bash_aliases /home/${1}/.bash_aliases
+ln -sf /srv/git/d4rks-dotfiles/.bash_functions /home/${1}/.bash_functions
+ln -sf /srv/git/d4rks-dotfiles/.bash_lessfilter /home/${1}/.bash_lessfilter
+ln -sf /srv/git/d4rks-dotfiles/.bash_profile /home/${1}/.bash_profile
+ln -sf /srv/git/d4rks-dotfiles/.bashrc /home/${1}/.bashrc
+ln -sf /srv/git/d4rks-dotfiles/.makepkg.i686.conf /home/${1}/.makepkg.i686.conf
+ln -sf /srv/git/d4rks-dotfiles/.vimrc /home/${1}/.vimrc
 
-ln -s /srv/git/d4rks-dotfiles/.bash_aliases /home/${1}/.bash_aliases
-ln -s /srv/git/d4rks-dotfiles/.bash_functions /home/${1}/.bash_functions
-ln -s /srv/git/d4rks-dotfiles/.bash_lessfilter /home/${1}/.bash_lessfilter
-ln -s /srv/git/d4rks-dotfiles/.bash_profile /home/${1}/.bash_profile
-ln -s /srv/git/d4rks-dotfiles/.bashrc /home/${1}/.bashrc
-ln -s /srv/git/d4rks-dotfiles/.makepkg.i686.conf /home/${1}/.makepkg.i686.conf
-ln -s /srv/git/d4rks-dotfiles/.vimrc /home/${1}/.vimrc
-
-ln -s /srv/git/d4rks=dotfiles/configs/cryptboot/98-cryptboot-pacman.hook /etc/pacman.d/hooks/98-cryptboot-pacman.hook
+ln -sf /srv/git/d4rks=dotfiles/configs/cryptboot/98-cryptboot-pacman.hook /etc/pacman.d/hooks/98-cryptboot-pacman.hook
