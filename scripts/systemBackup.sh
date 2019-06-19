fde452d957420c2a5021a33fb31f0ef95e3f036c
David Hardin <dhardin@hardinsolutions.net>
2019-06-19 11:04:10 -0400

    Updated the system backup script to exclude the git directory and the owncloud directories.

--- scripts/systemBackup.sh
+++ scripts/systemBackup.sh
@@ -1,11 +1,13 @@
 backupsDir="/srv/backups"
 backupsStagingDir="${backupsDir}/stagingdir"
+owncloudDir="/home/dhardin/.ownCloud"
+gitDir="/srv/git"
 
 hostname=`hostname`
 timestamp=`date -Is`
 filename="sysBackup-${hostname}-${timestamp}.tar.gz"
 
 mkdir -p $backupsStagingDir
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found",$backupsDir} / $backupsStagingDir
+rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found",$backupsDir,$owncloudDir,$gitDir} / $backupsStagingDir
 tar -cvpzf "${backupsDir}/${filename}.tar.gz" --exclude="${backupsDir}/${filename}.tar.gz" $backupsStagingDir
 rm -Rf "${backupsStagingDir}"
