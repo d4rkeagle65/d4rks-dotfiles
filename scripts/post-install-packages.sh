9b83a43c2fa26105c863a9d73971f5b915c08579
David Hardin <dhardin@hardinsolutions.net>
2019-06-25 12:01:26 -0400

    Fixed a type on the post-install-packages script.

--- scripts/post-install-packages.sh
+++ scripts/post-install-packages.sh
@@ -3,5 +3,6 @@
 pkgs=$(</srv/git/d4rks-dotfiles/configs/package_install_list.txt)
 sudo pacman --noconfirm -S $pkgs
 
-owncloudcmd -h --user dhardin --password $1 --non-interactive --trust $HOME/.ownCloud/Davids/.laptopSetup/ https://owncloud.axxiscom.com/remote.php/webdav/Davids/.laptopShare/
+mkdir -p /home/dhardin/.ownCloud/Davids/.laptopSetup/
+owncloudcmd -h --user dhardin --password ${1} --non-interactive --trust $HOME/.ownCloud/Davids/.laptopSetup/ https://owncloud.axxiscom.com/remote.php/webdav/Davids/.laptopSetup/
 sh /home/dhardin/.ownCloud/Davids/.laptopSetup/owncloud_laptopSetupScript.sh
