5017294883944cf47928d4393a91c5e93f53ee22
d4rkeagle65 <50802414+d4rkeagle65@users.noreply.github.com>
2019-06-27 09:06:04 -0400

    Update dotfiles-setup.sh

--- dotfiles-setup.sh
+++ dotfiles-setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+[ -d /home/${1}/.local ] || mkdir -p /home/${1}/.local
 mkdir -p /home/${1}/.local/share
 mkdir -p /home/${1}/.config
 chown ${1}.users -R /home/${1}/.local
@@ -16,24 +17,24 @@ ln -sf /srv/git/d4rks-dotfiles/.xinitrc /home/${1}/.xinitrc
 
 ln -sf /srv/git/d4rks-dotfiles/configs/termite/termite /home/${1}/.config/termite
 
-sudo mkdir -p /etc/pacman.d/hooks
+[ -d /etc/pacman.d/hooks ] || sudo mkdir -p /etc/pacman.d/hooks
 sudo ln -sf /srv/git/d4rks-dotfiles/configs/cryptboot/98-cryptboot-pacman.hook /etc/pacman.d/hooks/98-cryptboot-pacman.hook
 
 mkdir -p /home/${1}/.config/conky
-chown ${1}.users /home/${1}/.config/conky
+chown -R ${1}.users /home/${1}/.config/conky
 ln -sf /srv/git/d4rks-dotfiles/configs/conky/conky.conf /home/${1}/.config/conky/conky.conf
 ln -sf /srv/git/d4rks-dotfiles/configs/conky/today.py /home/${1}/.config/conky/today.py
 
 mkdir -p /home/${1}/.config/ownCloud
-chown ${1}.users /home/${1}/.config/ownCloud
+chown -R ${1}.users /home/${1}/.config/ownCloud
 ln -sf /srv/git/d4rks-dotfiles/configs/ownCloud/owncloud.cfg /home/${1}/.config/ownCloud/owncloud.cfg
 
 mkdir -p /home/${1}/.config/i3
-chown ${1}.users /home/${1}/.config/i3
+chown -R ${1}.users /home/${1}/.config/i3
 ln -sf /srv/git/d4rks-dotfiles/configs/i3/config /home/${1}/.config/i3/config
 
 mkdir -p /home/${1}/.config/trizen
-chown ${1}.users /home/${1}/.config/trizen
+chown -R ${1}.users /home/${1}/.config/trizen
 ln -sf /srv/git/d4rks-dotfiles/configs/trizen/trizen.conf /home/${1}/.config/trizen/trizen.conf
 
-chown ${1}.users /home/${1}/.config
+chown -R ${1}.users /home/${1}/.config
