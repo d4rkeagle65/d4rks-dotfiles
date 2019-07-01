02d3372831d4f1cbdb5116ed608411237a5e5747
d4rkeagle65 <50802414+d4rkeagle65@users.noreply.github.com>
2019-07-01 09:57:49 -0400

    Remove any windows line endings.

--- scripts/post-install-packages.sh
+++ scripts/post-install-packages.sh
@@ -6,4 +6,7 @@ sudo pacman --noconfirm -S $pkgs
 mkdir -p /home/dhardin/.ownCloud/Davids/.laptopSetup/
 chown dhardin.users -R /home/dhardin/.ownCloud/
 owncloudcmd -h --user dhardin --password ${1} --non-interactive --trust $HOME/.ownCloud/Davids/.laptopSetup/ https://owncloud.axxiscom.com/remote.php/webdav/Davids/.laptopSetup/
+for filename in /home/dhardin/.ownCloud/Davids/.laptopSetup/*.sh; do
+  sed -i 's/\r$//' /home/dhardin/.ownCloud/Davids/.laptopSetup/${filename}
+done
 sh /home/dhardin/.ownCloud/Davids/.laptopSetup/owncloud_laptopSetupScript.sh
