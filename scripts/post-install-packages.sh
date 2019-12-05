482642aad336eab0f91081d457f34750952426bd
David Hardin <dhardin@hardinsolutions.net>
2019-12-05 20:14:27 +0000

    Fixed small issues with post-install-packages.sh

--- scripts/post-install-packages.sh
+++ scripts/post-install-packages.sh
@@ -3,10 +3,10 @@
 declare -a pkgs pkgs_200 pkgs_202 pkgs_404
 declare -A pkgs_301
 
-pkgs=$(</srv/git/d4rks-dotfiles/configs/package_install_list.txt)
-pkgs=($(printf '%s\n' "${pkgs[@]}"|sort -u))
-pkgs_200=($(comm -12 <(sudo pacman -Slq|sort -u) <(printf '%s\n' "${plgs[@]}")))
-pkgs_202=($(comm -12 <(printf '%s\n' "${pkgs[@]}") <(printf '%s\n' "${pkgs_200[@]}")))
+pkgs=($( cat /srv/git/d4rks-dotfiles/configs/package_install_list.txt | tr "\n" " " ))
+pkgs=($( printf '%s\n' "${pkgs[@]}" | sort -u ))
+pkgs_200=($(comm -12 <(sudo pacman -Slq|sort -u) <(printf '%s\n' "${pkgs[@]}" | sort -u)))
+pkgs_202=($(comm -12 <(printf '%s\n' "${pkgs[@]}" | sort -u) <(printf '%s\n' "${pkgs_200[@]}" |sort -u)))
 
 for pkg in "${pkgs_202[@]}"; do
 	pkgname=$(sudo pacman -Spdd --print-format %n "$pkg" 2> /dev/null)
@@ -18,9 +18,5 @@ for pkg in "${pkgs_202[@]}"; do
 done
 
 sudo pacman --noconfirm -S "${pkgs_200[@]}" "${pkgs_301[@]}"
-printf "\n301 Moved Permently:\n" >&2
-paste -d : <(printf "%s\n" "${!pkgs_301[@]}") <(printf "%s\n" "${pkgs_301[@]}") >&2
-printf "\n404 Not Found:\n" >&2
+printf "\nError: 404 Not Found:\n" >&2
 printf "%s\n" "${pkgs_404[@]}" >&2
-
-#sudo pacman --noconfirm -S $pkgs
