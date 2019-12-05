3a95367772ada1b8095e3b0aefcd247dbe0a762f
David Hardin <dhardin@hardinsolutions.net>
2019-12-05 16:37:31 +0000

    New post-install package script

--- scripts/post-install-packages.sh
+++ scripts/post-install-packages.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+declare -a pkgs pkgs_200 pkgs_202 pkgs_404
+declare -A pkgs_301
+
 pkgs=$(</srv/git/d4rks-dotfiles/configs/package_install_list.txt)
-sudo pacman --noconfirm -S $pkgs
+pkgs=($(printf '%s\n' "${pkgs[@]}"|sort -u))
+pkgs_200=($(comm -12 <(sudo pacman -Slq|sort -u) <(printf '%s\n' "${plgs[@]}")))
+pkgs_202=($(comm -12 <(printf '%s\n' "${pkgs[@]}") <(printf '%s\n' "${pkgs_200[@]}")))
+
+for pkg in "${pkgs_202[@]}"; do
+	pkgname=$(sudo pacman -Spdd --print-format %n "$pkg" 2> /dev/null)
+	if [[ -n $pkgname ]]; then
+		pkgs_301[$pkg]=$pkgname
+	else
+		pkgs_404+=("$pkg")
+	fi
+done
+
+sudo pacman --noconfirm -S "${pkgs_200[@]}" "${pkgs_301[@]}"
+printf "\n301 Moved Permently:\n" >&2
+paste -d : <(printf "%s\n" "${!pkgs_301[@]}") <(printf "%s\n" "${pkgs_301[@]}") >&2
+printf "\n404 Not Found:\n" >&2
+printf "%s\n" "${pkgs_404[@]}" >&2
+
+#sudo pacman --noconfirm -S $pkgs
